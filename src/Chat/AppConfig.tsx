ae44b14814b985c8a5e9e275f6608b106382cf80
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-07 20:11:38 +0200

    feat: Store config to localstorage.

--- src/Chat/AppConfig.tsx
+++ src/Chat/AppConfig.tsx
@@ -6,52 +6,95 @@ interface IAppConfig {
   setClientConfig: Dispatch<SetStateAction<IClientConfig>>;
 }
 
+const localStorageGondulAPI = "pg-gondul";
+const localStorageGondulCredentials = "pg-gondul-credentials";
+
+export function getFromLocalStorage(): IClientConfig {
+  const gondul = window.localStorage.getItem(localStorageGondulAPI);
+  const credentials = window.localStorage.getItem(
+    localStorageGondulCredentials
+  );
+  return {
+    Credentials: credentials || "",
+    Gondul: gondul || "",
+  };
+}
+function setLocalStorage(clientConfig: IClientConfig) {
+  window.localStorage.setItem(localStorageGondulAPI, clientConfig.Gondul);
+  window.localStorage.setItem(
+    localStorageGondulCredentials,
+    clientConfig.Credentials
+  );
+}
+function clearLocalStorage() {
+  window.localStorage.setItem(localStorageGondulAPI, "");
+  window.localStorage.setItem(localStorageGondulCredentials, "");
+}
+
 function AppConfig(props: IAppConfig) {
-  const {clientConfig, setClientConfig} = props;
+  const { clientConfig, setClientConfig } = props;
   const [gondul, setGondul] = useState(clientConfig.Gondul);
   const [user, setUser] = useState(
     atob(clientConfig.Credentials).split(":")[0]
   );
 
-  const [pass, setPass] = useState(atob(clientConfig.Credentials).split(":")[1]);
-    return (
-        <form action="">
-          <div>
-            <label htmlFor="gondulApi">Gondul hostname (https://</label>
-            <input
-              value={new URL(gondul).host}
-              onChange={e => setGondul(`https://${e.target.value}`)}
-              name="gondulApi"
-            />
-            <span>/api/)</span>
-          </div>
-          <div>
-            <label htmlFor="gondulUser">Username</label>
-            <input
-              value={user}
-              onChange={e => setUser(e.target.value)}
-              name="gondulUser"
-            />
-          </div>
-          <div>
-            <label htmlFor="gondulPass">Password</label>
-            <input
-              value={pass}
-              onChange={e => setPass(e.target.value)}
-              name="gondulPass"
-              type="password"
-            />
-          </div>
-          <button type="submit" onClick={e => {
-            e.preventDefault();
-            setClientConfig({
-                ...clientConfig,
-                Credentials: (user && pass) ? btoa(`${user}:${pass}`) : clientConfig.Credentials,
-                Gondul: gondul ? gondul : clientConfig.Gondul,
-            })
-          }}>Lagre</button>
-        </form>
-    )
+  const [pass, setPass] = useState(
+    atob(clientConfig.Credentials).split(":")[1]
+  );
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="gondulApi">Gondul hostname (https://</label>
+        <input
+          value={gondul ? new URL(gondul).host : ""}
+          onChange={e => setGondul(`https://${e.target.value}`)}
+          name="gondulApi"
+        />
+        <span>/api/)</span>
+      </div>
+      <div>
+        <label htmlFor="gondulUser">Username</label>
+        <input
+          value={user}
+          onChange={e => setUser(e.target.value)}
+          name="gondulUser"
+        />
+      </div>
+      <div>
+        <label htmlFor="gondulPass">Password</label>
+        <input
+          value={pass}
+          onChange={e => setPass(e.target.value)}
+          name="gondulPass"
+          type="password"
+        />
+      </div>
+      <button
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          const config = {
+            ...clientConfig,
+            Credentials:
+              user && pass ? btoa(`${user}:${pass}`) : clientConfig.Credentials,
+            Gondul: gondul ? gondul : clientConfig.Gondul,
+          };
+          setClientConfig(config);
+          setLocalStorage(config);
+        }}
+      >
+        Lagre
+      </button>
+      <button
+        onClick={e => {
+          e.preventDefault();
+          clearLocalStorage();
+        }}
+      >
+        Slett lagret informasjon
+      </button>
+    </form>
+  );
 }
 
 export default AppConfig;
--- src/Chat/index.tsx
+++ src/Chat/index.tsx
@@ -1,8 +1,8 @@
 import React, { Dispatch, SetStateAction, useState } from "react";
 import { hot } from "react-hot-loader";
 
-import AppConfig from "./AppConfig";
-import { ContextProvider, defaultClientConfig } from "./ClientConfig";
+import AppConfig, { getFromLocalStorage } from "./AppConfig";
+import { ContextProvider } from "./ClientConfig";
 import { MessagesContainer } from "./Components/Messages";
 
 type AppId = "OPLOG";
@@ -19,7 +19,7 @@ function MessagesApp() {
   const [appId, _]: [AppId, Dispatch<SetStateAction<AppId>>] = useState(
     "OPLOG" as AppId
   );
-  const [clientConfig, setClientConfig] = useState(defaultClientConfig);
+  const [clientConfig, setClientConfig] = useState(getFromLocalStorage());
   const [showConfig, setShowConfig] = useState(false);
 
   const ActiveApp = getActiveApp(appId);
