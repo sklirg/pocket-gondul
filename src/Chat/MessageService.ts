604cb9079f00dc920f5d6851c55c51c7211fe88c
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-07 23:25:15 +0200

    fix: Remove "'s in the systems response.

--- src/Chat/MessageService.ts
+++ src/Chat/MessageService.ts
@@ -50,7 +50,7 @@ export function convertOplogResponseToMessages(
   return data.oplog.map(entry => ({
     message: entry.log,
     sender: entry.username,
-    systems: entry.systems.split(","),
+    systems: entry.systems.split(",").map(system => system.replace(/"/g, "")),
     time: new Date(entry.timestamp),
   }));
 }
