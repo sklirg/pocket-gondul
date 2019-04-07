f1ee60b192e8d4e8a57245676c400d86254f6b33
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-07 16:04:45 +0200

    fix: Only use mocked messages if explicitly defined.

--- src/Chat/MessageService.ts
+++ src/Chat/MessageService.ts
@@ -37,7 +37,7 @@ async function fetchMessages(): Promise<IOPLogResponse> {
 }
 
 async function getMessages(): Promise<IMessage[]> {
-  if (process.env.PG_USE_MOCKED_MESSAGES) {
+  if (process.env.PG_USE_MOCKED_MESSAGES === "true") {
     return Promise.resolve(mockedMessages);
   }
   return convertOplogResponseToMessages(await fetchMessages());
--- src/Types.d.ts
+++ src/Types.d.ts
@@ -1,10 +1,12 @@
+export {};
+
 declare const process: {
   env: {
     NODE_ENV: string;
     GONDUL_HOST: string;
     GONDUL_USER: string;
     GONDUL_PASS: string;
-    PG_USE_MOCKED_MESSAGES: boolean;
+    PG_USE_MOCKED_MESSAGES: string;
   };
 };
 
