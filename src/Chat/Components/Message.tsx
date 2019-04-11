6e294dcd339d6b16bdb62ee98fe04deb4928089f
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-11 21:52:20 +0200

    chore: Refactor Message to function component.

--- src/Chat/Components/Message.tsx
+++ src/Chat/Components/Message.tsx
@@ -19,32 +19,27 @@ interface IProps extends IMessage {
   onClickSystem: (system: string) => void;
 }
 
-class Message extends React.PureComponent<IProps> {
-  public render() {
-    const timestring = format(this.props.time, "yyyy-MM-dd HH:mm:ss", {
-      locale: nb,
-    });
+function Message(props: IProps) {
+  const timestring = format(props.time, "yyyy-MM-dd HH:mm:ss", {
+    locale: nb,
+  });
 
-    return (
-      <div className="message">
-        <div className="message--time">{timestring}</div>
-        <div className="message--sender">- {this.props.sender}</div>
-        <div className="message--systems">
-          {this.props.systems.map(system => (
-            <button
-              key={system}
-              onClick={() => this.props.onClickSystem(system)}
-            >
-              {system}
-            </button>
-          ))}
-        </div>
-        <div className="message--message">
-          {this.props.message.replace(errorMessagePrefix, "")}
-        </div>
+  return (
+    <div className="message">
+      <div className="message--time">{timestring}</div>
+      <div className="message--sender">- {props.sender}</div>
+      <div className="message--systems">
+        {props.systems.map(system => (
+          <button key={system} onClick={() => props.onClickSystem(system)}>
+            {system}
+          </button>
+        ))}
       </div>
-    );
-  }
+      <div className="message--message">
+        {props.message.replace(errorMessagePrefix, "")}
+      </div>
+    </div>
+  );
 }
 
 export default Message;
