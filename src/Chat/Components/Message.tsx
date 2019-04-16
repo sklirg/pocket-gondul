e70c04e9739dd2bf83705bf7c13b50b6ce1bc54d
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-17 00:13:36 +0200

    feat: Make it easier to select the system if multiple.

--- src/Chat/Components/Message.tsx
+++ src/Chat/Components/Message.tsx
@@ -30,11 +30,21 @@ function Message(props: IProps) {
       <div className="message--time">{timestring}</div>
       <div className="message--sender">- {props.sender}</div>
       <div className="message--systems">
-        {props.systems.map(system => (
-          <button key={system} onClick={() => props.onClickSystem(system)}>
-            {system}
-          </button>
-        ))}
+        {props.systems.length > 1 ? (
+          <select onChange={event => props.onClickSystem(event.target.value)}>
+            {props.systems.map(system => (
+              <option key={system} value={system}>
+                {system}
+              </option>
+            ))}
+          </select>
+        ) : (
+          props.systems.map(system => (
+            <button key={system} onClick={() => props.onClickSystem(system)}>
+              {system}
+            </button>
+          ))
+        )}
       </div>
       <div className="message--message">
         {props.message.replace(errorMessagePrefix, "")}
